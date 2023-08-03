e85556f37979af2ea2a2ffa3fed7a5b8acbe4596
Jason Eveleth <jj11@eveleth.com>
2023-08-03 17:18:51 -0400

    age gender; detects emotions

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 source: https://github.com/spmallick/learnopencv/tree/master/AgeGender
+source: https://github.com/habom2310/People-tracking-with-Age-and-Gender-detection/issues/1
+
+if it detects a man, it tries to guess emotion, if it detects a woman it says "wayy to hot"
+
+https://github.com/serengil/deepface
+https://github.com/ageitgey/face_recognition
--- detect.py
+++ detect.py
@@ -1,4 +1,5 @@
 import cv2
+from deepface import DeepFace
 
 def getFaceBox(net, frame, conf_threshold=0.7):
     frameOpencvDnn = frame.copy()
@@ -20,16 +21,50 @@ def getFaceBox(net, frame, conf_threshold=0.7):
             cv2.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight/150)), 8)
     return frameOpencvDnn, bboxes
 
-cam = cv2.VideoCapture(0)
+# continuity camera is usually first, so try to skip it
+cam = cv2.VideoCapture(1)
+if not cam.read()[0]:
+    cam = cv2.VideoCapture(0)
+
+genderProto = "gender_deploy.prototxt"
+genderModel = "gender_net.caffemodel"
+genderList = ['Male', 'Female']
+genderNet = cv2.dnn.readNet(genderModel, genderProto)
+genderNet.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+genderNet.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+
 faceProto = "opencv_face_detector.pbtxt"
 faceModel = "opencv_face_detector_uint8.pb"
-
 faceNet = cv2.dnn.readNet(faceModel, faceProto)
 
 while(True):
     ret, frame = cam.read()
 
     frameFace, bboxes = getFaceBox(faceNet, frame)
+    for bb in bboxes:
+        padding = 200
+        face80 = frame[max(0,bb[1]-padding):min(bb[3]+padding,frame.shape[0]-1),max(0,bb[0]-padding):min(bb[2]+padding, frame.shape[1]-1)]
+        emotion = ""
+        try:
+            # we assume there's only one
+            firstface = DeepFace.analyze(face80, actions=['emotion'])[0]
+            output = firstface['emotion']
+            emotion = max(output, key=output.get)
+        except ValueError as e:
+            print(e)
+
+        padding = 20
+        face20 = frame[max(0,bb[1]-padding):min(bb[3]+padding,frame.shape[0]-1),max(0,bb[0]-padding):min(bb[2]+padding, frame.shape[1]-1)]
+        # opencv is BGR not RGB, so they have this handy option that we want off
+        blob = cv2.dnn.blobFromImage(face20, 1.0, (227, 227), swapRB=False)
+        genderNet.setInput(blob)
+        genderPreds = genderNet.forward()
+        gender = genderList[genderPreds[0].argmax()]
+        if gender == 'Female':
+            emotion = "Wayy too hot"
+
+        label = f"{emotion}"
+        cv2.putText(frameFace, label, (bb[0], bb[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2, cv2.LINE_AA)
     cv2.imshow('frame', frameFace)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
