90d0170ec6133ee90cd0a44ee6a0eac1cf962e1c
Jason Eveleth <jj11@eveleth.com>
2023-08-03 02:44:30 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+source: https://github.com/spmallick/learnopencv/tree/master/AgeGender
---
+++ detect.py
@@ -0,0 +1,40 @@
+import cv2
+
+def getFaceBox(net, frame, conf_threshold=0.7):
+    frameOpencvDnn = frame.copy()
+    frameHeight = frameOpencvDnn.shape[0]
+    frameWidth = frameOpencvDnn.shape[1]
+    blob = cv2.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
+
+    net.setInput(blob)
+    detections = net.forward()
+    bboxes = []
+    for i in range(detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence > conf_threshold:
+            x1 = int(detections[0, 0, i, 3] * frameWidth)
+            y1 = int(detections[0, 0, i, 4] * frameHeight)
+            x2 = int(detections[0, 0, i, 5] * frameWidth)
+            y2 = int(detections[0, 0, i, 6] * frameHeight)
+            bboxes.append([x1, y1, x2, y2])
+            cv2.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight/150)), 8)
+    return frameOpencvDnn, bboxes
+
+cam = cv2.VideoCapture(0)
+faceProto = "opencv_face_detector.pbtxt"
+faceModel = "opencv_face_detector_uint8.pb"
+
+faceNet = cv2.dnn.readNet(faceModel, faceProto)
+
+while(True):
+    ret, frame = cam.read()
+
+    frameFace, bboxes = getFaceBox(faceNet, frame)
+    cv2.imshow('frame', frameFace)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
+
